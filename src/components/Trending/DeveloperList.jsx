d94b8a4ee3ee0f452e86a0eabaf869aeafd4ac08
rocksama <767444690@qq.com>
2017-06-17 20:16:42 +0800

    update: :rocket: user following stored and applying

--- src/components/Trending/DeveloperList.jsx
+++ src/components/Trending/DeveloperList.jsx
@@ -3,11 +3,13 @@ import {connect} from 'react-redux'
 import {fetchTrending} from "../../views/TrendingRedux"
 import {Link} from "react-router-dom";
 import {Button, Icon, Tooltip} from "antd";
+import Following from "../Common/Following"
 import "./List.scss"
 
 @connect((state=>({
 	route: state.common.route,
-	trending: state.trending
+	trending: state.trending,
+	followingList: state.common.following
 })), {fetchTrending})
 export default class DeveloperList extends Component {
 	componentDidMount(){
@@ -16,7 +18,7 @@ export default class DeveloperList extends Component {
 	}
 
 	render(){
-		const {language, span, trending} = this.props
+		const {language, span, trending, followingList} = this.props
 		let content = <p>123</p>
 
 		if(trending.status === 0){
@@ -31,7 +33,7 @@ export default class DeveloperList extends Component {
 					<Icon type="loading" />
 				</section>
 		}else if(trending.status === 2){
-			content = <List data={trending.result} />
+			content = <List data={trending.result} list={followingList} />
 		}
 
 		return content
@@ -41,30 +43,19 @@ export default class DeveloperList extends Component {
 export class List extends PureComponent {
 
 	render(){
-		const {data, simple= false} = this.props
-		return renderList({data, simple})
+		const {data, simple= false, list} = this.props
+		return renderList({data, simple, list})
 	}
 }
 
-function renderList({data, simple}) {
+function renderList({data, simple, list}) {
 	return (
 		<section className="developer-list">
 			<ul>
 				{
 					data.map((item, index)=>{
-
 						return (
-							<li key={index}>
-								<img src={item.avatar || item.avatar_url} alt=""/>
-								<h4>
-									<Link to={`/user/${item.name || item.login}/profile`} style={{fontSize: 16}}>{item.name || item.login}</Link>
-									{item.full_name ? `(${item.full_name})` : ''}
-									<br/>
-									<Link to={`/repo/${item.name || item.login}/${item.repo}`}>{item.repo}</Link>
-								</h4>
-								{simple ? '' : <p>{item.description}</p>}
-								{item.type === 'User' ? <Button className="star" key={item.name} size="small" ><Icon type="user" />Follow</Button> : ''}
-							</li>
+							<Following key={index} item={item} simple={simple} list={list}/>
 						)
 					})
 				}
